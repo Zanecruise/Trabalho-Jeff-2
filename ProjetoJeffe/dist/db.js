cf5a09d717fdc14d7f47065d319b9ab8c2fcfaac
Patrick <48337395+Zanecruise@users.noreply.github.com>
2024-10-28 22:05:52 -0300

    Update db.js

--- ProjetoJeffe/dist/db.js
+++ ProjetoJeffe/dist/db.js
@@ -3,11 +3,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// db.ts
 const sqlite3_1 = __importDefault(require("sqlite3"));
 const sqlite_1 = require("sqlite");
 const path_1 = __importDefault(require("path"));
-const DB_PATH = path_1.default.resolve(__dirname, 'bancoJeffe.sqlite'); // Altere 'database.db' para o nome do seu banco de dados
+// Usar process.cwd() para obter o diretório de trabalho atual
+const DB_PATH = path_1.default.resolve(process.cwd(), 'bancoJeffe.sqlite');
 const dbPromise = (0, sqlite_1.open)({
     filename: DB_PATH,
     driver: sqlite3_1.default.Database,
